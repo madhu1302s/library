677dddef06bd82e3a6b8800b8984ac1675f685d0
madhu1302s <166099310+madhu1302s@users.noreply.github.com>
2024-04-30 14:37:31 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -3,14 +3,4 @@ def gitDownload(repo)
     git "https://github.com/intelliqittrainings/${repo}.git" 
 }
 
-def mavenbuild()
-{
-    sh 'mvn package'
-}
-
-def tomcatdeploy(jobname,ip,context)
-{
-sh "scp /home/ubuntu/.Jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapp/${context}.war"
-}
-
 
